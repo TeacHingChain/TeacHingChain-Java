0d6961dc0b58050d73feb8209b0a015ffee7745e
Brett Hufnagle <TeacHingChain0420@gmail.com>
2019-05-04 17:35:58 -0400

    removing example code from ChainClientEndpoint

--- src/main/java/com/thc/blockchain/network/ChainClientEndpoint.java
+++ src/main/java/com/thc/blockchain/network/ChainClientEndpoint.java
@@ -1,24 +1,15 @@
 package com.thc.blockchain.network;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.websocket.*;
 
 @ClientEndpoint
 public class ChainClientEndpoint {
+
     @OnOpen
     public void onOpen(Session session) {
 
             System.out.println("Connected to endpoint: " + session.getBasicRemote());
-            try {
-                String name = "Duke";
-                System.out.println("Sending message to endpoint: " + name);
-                session.getBasicRemote().sendText(name);
-            } catch (IOException ex) {
-                Logger.getLogger(ChainClientEndpoint.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+    }
 
         @OnMessage
         public void processMessage(String message) {
