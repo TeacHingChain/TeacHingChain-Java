51c8224084c879debdc76b5181e25bfcd4fc814f
Brett Hufnagle <TeacHingChain0420@gmail.com>
2019-06-05 20:51:22 -0400

    testing peer connectivity verification, NOT STABLE

--- src/main/java/com/thc/blockchain/network/nodes/client/endpoints/HelloClientEndpoint.java
+++ src/main/java/com/thc/blockchain/network/nodes/client/endpoints/HelloClientEndpoint.java
@@ -2,18 +2,21 @@ package com.thc.blockchain.network.nodes.client.endpoints;
 
 import com.thc.blockchain.network.decoders.BlockDecoder;
 import com.thc.blockchain.network.encoders.BlockEncoder;
+import com.thc.blockchain.network.nodes.NodeManager;
 
 import javax.websocket.ClientEndpoint;
 import javax.websocket.OnError;
 import javax.websocket.OnOpen;
+import javax.websocket.Session;
 
 
 @ClientEndpoint (encoders = BlockEncoder.class, decoders = BlockDecoder.class)
 public class HelloClientEndpoint {
 
     @OnOpen
-    public void onOpen() {
-        System.out.println("Was able to reach hello-server!\n");
+    public void onOpen(Session session) {
+        NodeManager.registerNode(session, "hello-client");
+        System.out.println("Hello from " + session.getUserProperties().get("id").toString());
     }
 
     @OnError
--- src/main/java/com/thc/blockchain/network/nodes/server/endpoints/HelloServerEndpoint.java
+++ src/main/java/com/thc/blockchain/network/nodes/server/endpoints/HelloServerEndpoint.java
@@ -1,9 +1,11 @@
 package com.thc.blockchain.network.nodes.server.endpoints;
 
 import com.thc.blockchain.network.Constants;
+import com.thc.blockchain.network.nodes.NodeManager;
 
 import javax.websocket.OnError;
 import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 
@@ -11,8 +13,9 @@ import javax.websocket.server.ServerEndpoint;
 public class HelloServerEndpoint {
 
     @OnOpen
-    public void onOpen () {
-        System.out.println("Hello from client!\n");
+    public void onOpen (Session session) {
+        NodeManager.registerNode(session, "hello-server");
+        System.out.println("Hello from " + session.getUserProperties().get("id").toString());
     }
 
     @OnError
